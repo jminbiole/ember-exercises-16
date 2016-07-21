c7a2f1eb59055d16ee56c0209bde623318d9a426
Jeff Minbiole <minbiole.jeff@gmail.com>
2016-07-21 09:40:17 -0500

    don know when I saved last

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+  infoInput() {
+    const data = {
+      first: data.first,
+      last: data.last,
+      address: data.address,
+      phone: data.phone,
+    };
+
+    fetch(`https://tiny-tn.herokuapp.com/collections/jm-people`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'post',
+      body: JSON.stringify(data),
+    });
+  }
+}
 });
