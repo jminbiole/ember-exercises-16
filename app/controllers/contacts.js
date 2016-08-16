d55d36b815e148ea9bf6035a2a4adcc087f4e164
Jeff Minbiole <minbiole.jeff@gmail.com>
2016-08-16 17:50:42 -0500

    got the contact toggle to work

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -18,52 +18,33 @@ export default Ember.Controller.extend({
           this.set('model', updatedList);
         });
       }
-    }
+    },
+    seeContacts() {
+      this.toggleProperty('showingContacts');
+    },
+    infoInput() {
+      const userInfo = {
+        first: this.first,
+        last: this.last,
+        address: this.address,
+        phone: this.phone,
+      };
+      if (userInfo.first === '' || userInfo.last === '' || userInfo.address === '' || userInfo.phone === '') {
+        alert('All fields are not filled in. Please make sure all are filled in.');
+        return;
+      }
+
+      fetch(`https://tiny-tn.herokuapp.com/collections/jm-people`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        method: 'post',
+        body: JSON.stringify(userInfo),
+      }) .then((res) => res.json())
+          .then((infoInput) => {
+            this.set('model', [...this.model, infoInput]);
+      });
+    },
   }
 });
-
-// export default Ember.Controller.extend({
-//   actions: {
-//     deleteCat(kitty) {
-//       if (confirm('Are you sure you want a cat right meow?')) {
-//         fetch('https://tiny-tn.herokuapp.com/collections/cats/' + kitty._id, {
-//           method: 'Delete',
-//         }).then(() => {
-//           const updatedList = this.model.filter((item) => {
-//             // How do we know which items to let through
-//             // Only let cats through that aren't the one we're deleting
-//             return item._id !== kitty._id;
-//           });
-//
-//           this.set('model', updatedList);
-//         })
-//       }
-//     },
-//     addCat() {
-//       const newObj = {
-//         name: this.name || '',
-//         claws: this.claws || false,
-//         color: this.color || '',
-//         likesPeople: this.likesPeople || false,
-//       };
-//
-//       if (newObj.name === '' || newObj.color === '') {
-//         alert('Cat\'s must have a name and color');
-//         return;
-//       }
-//
-//       console.log(newObj);
-//       fetch('https://tiny-tn.herokuapp.com/collections/cats', {
-//         method: 'post',
-//         body: JSON.stringify(newObj),
-//         headers: {
-//           Accept: 'application/json',
-//           'Content-Type': 'application/json',
-//         },
-//       }).then((res) => res.json())
-//         .then((cat) => {
-//           this.set('model', [...this.model, cat]);
-//         });
-//     }
-//   }
-// });
